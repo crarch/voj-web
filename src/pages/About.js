227f0f812da7246c611f67cf9896e880ff2af9bc
Chiro <chiro2001@163.com>
2021-10-07 22:30:05 +0800

    About.js Template

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,14 @@
+import { Button, Container, Typography } from "@material-ui/core";
+
 const About = () => {
-  return <div>关于</div>;
+  // return <div>关于</div>;
+  return <Container>
+    <Typography variant="h2">作者</Typography>
+    <Typography variant="body1" color="secondary">bilibili@嘉然今天吃什么</Typography>
+    <Button variant="outlined" color="primary" onClick={() => {
+      window.location.href = "https://space.bilibili.com/672328094";
+    }}>懂了吗，点击跳转</Button>
+  </Container>;
 };
 
 export default About;
