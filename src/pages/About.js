eced00688944d93a22f092f6732385595b7a465a
Chiro <chiro2001@163.com>
2021-10-10 21:32:17 +0800

    fixing: muilty testbenches

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,23 @@
+import React from "react";
 import { Button, Container, Typography } from "@material-ui/core";
 
 const About = () => {
-  // return <div>关于</div>;
+  const [clicked, setClicked] = React.useState(0);
   return <Container>
     <Typography variant="h2">作者</Typography>
-    <Typography variant="body1" color="secondary">bilibili@嘉然今天吃什么</Typography>
+    <Typography variant="body1" color="secondary">{["Chiro2001@163.com", "bilibili@嘉然今天吃什么"][clicked]};i@phyzait.moe</Typography>
     <Button variant="outlined" color="primary" onClick={() => {
-      window.location.href = "https://space.bilibili.com/672328094";
-    }}>懂了吗，点击跳转</Button>
+      if (!clicked) {
+        setClicked(1);
+        window.open("https://space.bilibili.com/672328094", "_blank");
+      } else {
+        window.open("https://phyzait.moe/", "_blank");
+        window.open("https://chiro.work/", "_blank");
+      }
+    }}>访问作者主页</Button>
 
     <Typography variant="h2">为什么UI这么丑/BUG这么多？</Typography>
-    <Typography variant="body1" color="secondary">因为作者不是全职开发这个OJ<br>这个OJ本来是软件开发与实践A的作业，然而软A大幅度改版，相当于这个程序全部木大了。现在作者用爱发电来维护这个项目。如果你觉得作者写的不好，欢迎交PR。<s>review都不review直接给merge，owner直接转给你都行</s></Typography>
+    <Typography variant="body1" color="secondary">因为作者不是全职开发这个OJ<br />这个OJ本来是软件开发与实践A的作业，然而软A大幅度改版，相当于这个程序全部木大了。现在作者用爱发电来维护这个项目。如果你觉得作者写的不好，欢迎交PR。<s>review都不review直接给merge，owner直接转给你都行</s></Typography>
 
   </Container>;
 };
